62ffa4fad5cb8af930acb8a74a4fb98dfef85794
Jay Liu <747973660@qq.com>
2024-08-04 02:58:09 +1200

    clean

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable perfectionist/sort-imports */
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import 'src/global.css';
 
 import { useScrollToTop } from 'src/hooks/use-scroll-to-top';
@@ -27,7 +26,6 @@ export default function App() {
       <ThemeProvider>
         <Router />
       </ThemeProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
